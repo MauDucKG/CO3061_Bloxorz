554d05ef2757eb57fe2317780b9f3f2d566a4779
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2023-03-26 08:09:30 +0700

    duc: fix UI display error

--- index.py
+++ index.py
@@ -82,8 +82,10 @@ def draw_map(screen, node: Node, state: State, resolution_width, resolution_heig
                                     (x+ang+w, y-10+h), (x+ang, y+h), (x+ang, y+h+10), (x+ang+w, y+h)])
                 pygame.draw.polygon(screen, (0, 0, 0), [
                                     (x+ang+w, y-10+h), (x+ang, y+h), (x+ang, y+h+10), (x+ang+w, y+h)], 1)
-                pygame.draw.line(screen, BLACK, (x-10, y), (x+ang+w, y-10+h), 3)
-                pygame.draw.line(screen, BLACK, (x-10+w, y-10), (x+ang, y+h), 3)
+                pygame.draw.line(screen, BLACK, (x-10, y),
+                                 (x+ang+w, y-10+h), 3)
+                pygame.draw.line(
+                    screen, BLACK, (x-10+w, y-10), (x+ang, y+h), 3)
             elif map[i][j] == 2:
                 color = RED
                 w, h = 40, 30
@@ -100,8 +102,10 @@ def draw_map(screen, node: Node, state: State, resolution_width, resolution_heig
                                     (x+ang+w, y-10+h), (x+ang, y+h), (x+ang, y+h+10), (x+ang+w, y+h)])
                 pygame.draw.polygon(screen, (0, 0, 0), [
                                     (x+ang+w, y-10+h), (x+ang, y+h), (x+ang, y+h+10), (x+ang+w, y+h)], 1)
-                pygame.draw.line(screen, WHITE, (x-10, y), (x+ang+w, y-10+h), 3)
-                pygame.draw.line(screen, WHITE, (x-10+w, y-10), (x+ang, y+h), 3)
+                pygame.draw.line(screen, WHITE, (x-10, y),
+                                 (x+ang+w, y-10+h), 3)
+                pygame.draw.line(
+                    screen, WHITE, (x-10+w, y-10), (x+ang, y+h), 3)
             elif map[i][j] == 5:
                 color = WHITE
                 w, h = 40, 30
@@ -221,6 +225,7 @@ def draw_map(screen, node: Node, state: State, resolution_width, resolution_heig
         y = startY+(i1*30)
         i1 += 1
 
+
 def test(levels_array, method_choice):
     testAlgo = int(input("Test level: 0, test algorithm: 1? "))
     if testAlgo:
@@ -243,20 +248,20 @@ def test(levels_array, method_choice):
             start = time.time()
             if method_choice == 1:
                 path, nNode, mem = dfs(level.state)
-                #memory = memory_usage(level.state)
-                #print("Memory usage:", memory, "B")
+                # memory = memory_usage(level.state)
+                # print("Memory usage:", memory, "B")
             elif method_choice == 0:
                 path, nNode, mem = bfs(level.state)
-                #memory = memory_usage(level.state)
-                #print("Memory usage:", memory, "B")
+                # memory = memory_usage(level.state)
+                # print("Memory usage:", memory, "B")
             elif method_choice == 2:
                 path, nNode, mem = AStarSearch(level.state)
-                #memory = memory_usage(level.state)
-                #print("Memory usage:", memory, "B")
+                # memory = memory_usage(level.state)
+                # print("Memory usage:", memory, "B")
             elif method_choice == 3:
                 path, nNode, mem = monteSearch(level.state)
-                #memory = memory_usage(level.state)
-                #print("Memory usage:", memory, "B")
+                # memory = memory_usage(level.state)
+                # print("Memory usage:", memory, "B")
 
             end = time.time()
 
@@ -264,15 +269,17 @@ def test(levels_array, method_choice):
 
             str_level = str(levels_array.index(level) + 1)
             success = str(level.state.board[data[1]][data[0]]
-                        == 4 and level.state.board[data[3]][data[2]] == 4)
+                          == 4 and level.state.board[data[3]][data[2]] == 4)
             if success == "True":
                 i += 1
-            
-            print("Level " + str_level + ": " + success + ", path length: ", len(path), ", So node da tim kiem: ", nNode, ", Time: ", round(end - start, 4), "s, Memory Usage: " , mem)
+
+            print("Level " + str_level + ": " + success + ", path length: ", len(path),
+                  ", So node da tim kiem: ", nNode, ", Time: ", round(end - start, 4), "s, Memory Usage: ", mem)
         print("So level success: " + str(i))
         print("Tong so level: " + str(len(levels_array)))
         input("Press enter to exit.")
     return
+
 
 def memory_usage(state):
     memory = 0
@@ -282,6 +289,7 @@ def memory_usage(state):
         # Cộng dồn kích thước vào biến memory
         memory += size
     return memory
+
 
 def testAStarSearch(levels_array):
     lev = int(input("Nhap level can test: "))
@@ -298,18 +306,21 @@ def testAStarSearch(levels_array):
                 endTime = time.time()
 
                 data = path[len(path) - 1].data
-                
 
                 success = str(levels_array[lev-1].state.board[data[1]][data[0]]
-                            == 4 and levels_array[lev-1].state.board[data[3]][data[2]] == 4)
+                              == 4 and levels_array[lev-1].state.board[data[3]][data[2]] == 4)
                 if success == "True" and endTime - startTime < minTime:
                     minTime = endTime - startTime
                     a, b, c = i, j, k
 
-                print("case a = {}, b = {}, c = {}: ".format(i, j, k) + success + ": " + str(round(endTime - startTime, 4)) + "s")
-                print("so node da tim kiem: ", nNode, ", path length: ", len(path))
+                print("case a = {}, b = {}, c = {}: ".format(i, j, k) +
+                      success + ": " + str(round(endTime - startTime, 4)) + "s")
+                print("so node da tim kiem: ", nNode,
+                      ", path length: ", len(path))
 
-    print("====== Best case: a = {}, b = {}, c = {}: finish in {}".format(a, b, c, minTime))
+    print("====== Best case: a = {}, b = {}, c = {}: finish in {}".format(
+        a, b, c, minTime))
+
 
 def testMonteCarlo(levels_array):
     lev = int(input("Nhap level can test: "))
@@ -319,21 +330,21 @@ def testMonteCarlo(levels_array):
     minTime = 99999
     bestRand = 0
     for i in range(startRnd, endRnd + 1, step):
-        
+
         startTime = time.time()
         path, nNode = monteSearch(levels_array[lev-1].state, i)
         endTime = time.time()
 
         data = path[len(path) - 1].data
-        
 
         success = str(levels_array[lev-1].state.board[data[1]][data[0]]
-                    == 4 and levels_array[lev-1].state.board[data[3]][data[2]] == 4)
+                      == 4 and levels_array[lev-1].state.board[data[3]][data[2]] == 4)
         if success == "True" and endTime - startTime < minTime:
             minTime = endTime - startTime
             bestRand = i
 
-        print("case i = {}: ".format(i) + success + ": " + str(round(endTime - startTime, 4)) + "s")
+        print("case i = {}: ".format(i) + success + ": " +
+              str(round(endTime - startTime, 4)) + "s")
         print("so node da tim kiem: ", nNode, ", path length: ", len(path))
 
     print("====== Best case: {} random: finish in {}".format(bestRand, minTime))
@@ -351,16 +362,16 @@ def main():
     level_choice = int(input("Nhap level: "))
     done = False
     if method_choice == 0:
-        path = bfs(levels_array[level_choice - 1].state)
+        path, nNode, mem = bfs(levels_array[level_choice - 1].state)
         algorithm = "Breadth-First Search"
     elif method_choice == 1:
-        path = dfs(levels_array[level_choice - 1].state)
+        path, nNode, mem = dfs(levels_array[level_choice - 1].state)
         algorithm = "Depth-First Search"
     elif method_choice == 2:
-        path = AStarSearch(levels_array[level_choice - 1].state)
+        path, nNode, mem = AStarSearch(levels_array[level_choice - 1].state)
         algorithm = "A* Search"
     elif method_choice == 3:
-        path = monteSearch(levels_array[level_choice - 1].state)
+        path, nNode, mem = monteSearch(levels_array[level_choice - 1].state)
         algorithm = "Monte Carlo Tree Search"
 
     pygame.init()
@@ -383,35 +394,35 @@ def main():
                     if i > 0:
                         i -= 1
         screen.fill(GREEN)
-        pygame.font.init()  
+        pygame.font.init()
         my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
-        text_surface = my_font.render(algorithm, False, (0, 0, 0))
+        text_surface = my_font.render(algorithm + " - level " +  str(level_choice), False, (0, 0, 0))
         screen.blit(text_surface, (10, 0))
 
         text_surface = my_font.render(
-            'Solution: ' + str(len(path)) + ' steps', False, (0, 0, 0))
+            'Solution: ' + str(len(path)) + " steps, Nodes searched: " + str(nNode) + " nodes, Memory Usage: " + str(mem) + " B", False, (0, 0, 0))
         screen.blit(text_surface, (10, resolution_height - 150))
 
         if i != 0:
             text_surface = my_font.render(
                 str(path[i-1].data), False, (128, 128, 128))
-            screen.blit(text_surface, (10, resolution_height - 100))
+            screen.blit(text_surface, (100 , resolution_height - 100))
 
             text_surface = my_font.render('->', False, (0, 0, 0))
-            screen.blit(text_surface, (210, resolution_height - 100))
+            screen.blit(text_surface, (310, resolution_height - 100))
 
         text_surface = my_font.render(
             str(path[i].data), False, (255, 255, 255))
-        screen.blit(text_surface, (270, resolution_height - 100))
+        screen.blit(text_surface, (370, resolution_height - 100))
 
         if i != len(path) - 1:
             text_surface = my_font.render('->', False, (0, 0, 0))
-            screen.blit(text_surface, (480, resolution_height - 100))
+            screen.blit(text_surface, (580, resolution_height - 100))
 
             text_surface = my_font.render(
                 str(path[i+1].data), False, (128, 128, 128))
-            screen.blit(text_surface, (540, resolution_height - 100))
+            screen.blit(text_surface, (640, resolution_height - 100))
 
         draw_map(screen, path[i], levels_array[level_choice -
                  1].state, resolution_width, resolution_height)
@@ -419,4 +430,4 @@ def main():
 
 
 main()
-#testAStarSearch()
+# testAStarSearch()
